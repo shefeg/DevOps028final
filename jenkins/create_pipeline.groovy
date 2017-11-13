209467166ebcee58e4a2abcf81d48d0cbcf312b3
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2017-11-13 14:00:59 +0200

    fixes

--- jenkins/create_pipeline.groovy
+++ jenkins/create_pipeline.groovy
@@ -5,4 +5,7 @@ def configXml = "/var/jenkins_home/build_application.xml"
 
 def xmlStream = new ByteArrayInputStream( configXml.getBytes() )
 
-Jenkins.instance.createProjectFromXML(jobName, xmlStream)
+//Jenkins.instance.createProjectFromXML(jobName, xmlStream)
+Job createPipeline {
+    parent.createProjectFromXML(jobName, xmlStream)
+}
