439b6e0d0fc2d187f4e632d1ea995ebc5582f520
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2017-11-15 16:31:25 +0200

    fixes

--- jenkins/create_pipeline.groovy
+++ jenkins/create_pipeline.groovy
@@ -1,3 +1,3 @@
-def cmd = "java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ -auth alexey:Ins1ght create-job build_application_4 < /var/jenkins_home/build_application.xml";
+def cmd = "java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ -auth alexey:Ins1ght create-job build_application < /var/jenkins_home/build_application.xml";
 ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
 pb.start();
